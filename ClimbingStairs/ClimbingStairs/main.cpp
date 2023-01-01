c12fde9ee09e0ac362c4474f3af146ad931a6760
orangery-me <102220086@sv1.dut.udn.vn>
2023-01-01 10:40:54 +0700

    31/12/2022
    update - 31/12/2022

---
+++ ClimbingStairs/ClimbingStairs/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  ClimbingStairs
+//
+//  Created by Đinh Bảo Châu Thi on 1/1/23.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+
+    long long factor(int k, int n){
+        long long res=1;
+        if (k==0)
+        return 1;
+
+        for (int i=1 ;i<=k ;++i,n--)
+            res = (res * n)/i;
+
+        return res;
+    
+    }
+    long long climbStairs(int n) {
+
+        long long sum=0;
+        for (int i=0; i<=n/2; ++i){
+            int k=(n-i);
+            sum+= factor(i,k);
+
+        }
+        return sum;
+    }
+};
+
+int main(){
+    int n; cin >>n;
+    Solution p;
+    cout <<p.climbStairs(n)<<endl;
+}
---
+++ RomanToInterger/RomanToInterger/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int cv(char a){
+        if (a=='I') return 1;
+        if (a=='V') return 5;
+        if (a=='X') return 10;
+        if (a=='L') return 50;
+        if (a=='C') return 100;
+        if (a=='D') return 500;
+         return 1000;
+    }
+    int romanToInt(string s) {
+        int n=s.size(),sum=0;
+        for (int i=0; i<n-1; ++i ) {
+            if ( cv(s[i]) <cv(s[i+1]) )
+            sum-= cv(s[i]);
+            else
+            sum+= cv(s[i]);
+            
+        }
+        sum+=cv(s[n-1]);
+        return sum;
+    }
+};
+
+int main(){
+    string s;
+    getline(cin,s);
+    Solution p;
+    cout <<p.romanToInt(s)<<endl;
+}
